47295ba6cef32dda4faf9e4100ecabd650f28e37
dam xuan bac <damxuanbachochbt10@gmail.com>
2026-06-01 22:31:53 +0700

    bt buoi 4

--- EX4/MVCTemplate006/MVCTemplate006/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MVCTemplate006.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
