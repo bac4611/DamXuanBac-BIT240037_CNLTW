e67822ec4c56b24f793fb92f9b7986a7aca3ce3c
dam xuan bac <damxuanbachochbt10@gmail.com>
2026-06-01 22:19:21 +0700

    bt buoi 4

---
+++ EX4/MVCTemplate006/MVCTemplate006/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCTemplate006.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Book
+{
+    public int id { get; set; }
+
+    
+    [Required(ErrorMessage = "Không được để trống tên sách!")]
+    public string name { get; set; } = string.Empty;
+
+   
+    [Required(ErrorMessage = "Vui lòng nhập giá sách!")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "Giá phải lớn hơn 0!")]
+    public decimal price { get; set; }
+}
